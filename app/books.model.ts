332246bd96af5f0d2f56ff8b70705c99ecdfdfa8
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-05-17 19:49:14 -0400

    Add files via upload
    this is a new project i was starting , about a little book app search

---
+++ app/books.model.ts
@@ -0,0 +1,13 @@
+export class Book{
+
+constructor(
+
+public id?:number,
+public name?:string,
+public writer?:string,
+public price?:number
+
+){
+
+}//end of the constructor
+}//end of the book class
---
+++ app/dataSource.model.ts
@@ -0,0 +1,11 @@
+import { Book } from './books.model';
+
+export class DataSource {
+
+private books: Book[];
+constructor(){
+this.books = new Array<Book>;( newBook(1,'War and peace','Tina',20))
+}
+
+
+}
